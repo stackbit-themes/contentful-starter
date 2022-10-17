5e6fee06670ba3b56e973c182fb927e79243865d
tomasbankauskas <tbdizainas@gmail.com>
2022-10-17 13:40:52 +0300

    added stackbit.config.js, updated package.json

---
+++ stackbit.config.js
@@ -0,0 +1,36 @@
+import path from 'path';
+import dotenv from 'dotenv';
+
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default {
+    stackbitVersion: '~0.5.0',
+    ssgName: 'nextjs',
+    nodeVersion: '16',
+
+    // contentSources is a list of modules implementing the ContentSourceInterface
+    contentSources: [
+        new ContentfulContentSource({
+            spaceId: process.env.CONTENTFUL_SPACE_ID,
+            environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
+            previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
+        })
+    ],
+
+    mapModels: ({ models, contentSourceType, contentSourceProjectId }) => {
+        return models.map((model) => {
+            // to enable page editing and sitemap features in Stackbit
+            // change the model type of the 'page' model to 'type: page'
+            if (model.name === 'page') {
+                model.type = 'page';
+                model.urlPath = '/{slug}';
+            }
+
+            return model;
+        });
+    }
+
+};
